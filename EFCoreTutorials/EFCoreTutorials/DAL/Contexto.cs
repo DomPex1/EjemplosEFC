5b35d5eec97e144cd5b4601e1701ae8bd81484e0
DomPex1 <71046476+DomPex1@users.noreply.github.com>
2020-09-27 08:37:21 -0400

    Add files via upload

---
+++ EFCoreTutorials/EFCoreTutorials/DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using EFCoreTutorials.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreTutorials.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
+
+    }
+}
---
+++ EFCoreTutorials/EFCoreTutorials/Models/Estudiante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreTutorials.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Course
+    {
+
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+
+    }
+}
